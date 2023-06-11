a81b3031a00ddc23dc0afb4404151d39662bab4f
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2023-06-11 15:40:40 +0600

    single  id related cars details server done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,9 +26,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    //  6 car all  information
     const sportsCollection = client.db("sportsCar").collection("sports");
     const regularCarCollection = client.db("regular-car").collection("regular");
-    const CarCollection = client.db("police-car").collection("car");
+    const CarCollection = client.db("police-car").collection("cars");
 
     app.get("/sports", async (req, res) => {
       const cursor = sportsCollection.find();
@@ -40,9 +41,53 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get("/car", async (req, res) => {
+    app.get("/cars", async (req, res) => {
       const cursor = CarCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Single Id 6 car information
+
+    app.get("/sports/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { name: 1, picture: 1, price: 1, rating: 1 },
+      };
+
+      const result = await sportsCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // regular
+    app.get("/regular/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { name: 1, picture: 1, price: 1, rating: 1 },
+      };
+
+      const result = await regularCarCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // car
+
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { name: 1, picture: 1, price: 1, rating: 1 },
+      };
+
+      const result = await CarCollection.findOne(query, options);
       res.send(result);
     });
 
