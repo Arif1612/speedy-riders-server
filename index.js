764d85a6a89fa8474d7f0685024b39afa8323652
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2023-06-11 06:13:26 +0600

    successfully connected to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,8 +8,42 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// speedy-rides
+// P8c3CB843hdpo3K5
+
+
+const uri = "mongodb+srv://speedy-rides:P8c3CB843hdpo3K5@cluster0.dy7mgpm.mongodb.net/?retryWrites=true&w=majority";
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dy7mgpm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+// main run
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("speedy rides server is running");
 });
 
 app.listen(port, () => {
